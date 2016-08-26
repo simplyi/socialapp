0451f98d1cc529458453f8167d23d14fb3efd82d
Sergey Kargopolov <skargopolov@central1.com>
2016-08-26 15:17:48 -0700

    Initial Commit

---
+++ SocialApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialApp
+//
+//  Created by Sergey Kargopolov on 2016-08-26.
+//  Copyright © 2016 Sergey Kargopolov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
